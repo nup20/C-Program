75d89fdbb7a7d7b7ce619e50f49e6346790f8540
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-07 20:34:44 +0530

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+top 2
+1) pre-define
+2) user define- function
+
+1) call by value  ->4
+
+a) no return no arg {  void  / () }
+b) no return but args { void  /(int a,float b,double c.....)
+c) return but no args { void -> int float... and ()}
+d) return and args { void  /(int a,float b,double c.....)
+*/
+
+
+
+
+void sum()
+{
+    int a,b,c;
+    printf("Enter any 2 number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("result = %d\n",c);
+
+}
+
+void sum1(int x,int y)
+{
+    int a,b,c;
+    printf("Enter any 2 number");
+    scanf("%d%d",&a,&b);
+    c=a+b+x+y;
+    printf("result = %d\n",c);
+
+}
+
+int sum2()
+{
+    int a,b,c;
+    printf("Enter any 2 number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("result = %d\n",c);
+   return c+3;
+}
+
+
+int sum3(int x,int y)
+{
+    int a,b,c;
+    printf("Enter any 2 number");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("result = %d\n",c);
+
+    return x+y;
+
+}
+
+int main()
+{
+int s;
+  sum();
+  sum1(5,1);
+  s= sum2();
+  printf("%d\n\n",s);
+
+  s= sum3(2,8);
+  printf("Sum =%d",s);
+
+    return 0;
+}
