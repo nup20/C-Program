2b7f884dd16d89487663be85068819cd099fa0da
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-07 20:36:54 +0530

    Add files via upload

---
+++ Array 3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int n,e[10],i,sum=0;
+    printf("Input The number o element to be stored in the array:");
+    scanf("%d",&n);
+    printf("Input %d Element in the array\n",n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Element-%d:",i);
+        scanf("%d",&e[i]);
+    }
+    for(i=0;i<n;i++)//c
+    {
+    sum+=e[i];
+    }
+    printf("Sum of all elements stored in the array is:%d",sum);
+
+    return 0;
+}
