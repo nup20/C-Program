dd5eca064c778d6b85a2a77b01b762a542d22efa
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-09 13:57:09 +0530

    Add files via upload

---
+++ Pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int m=10,n,o;
+int *z;
+z=m;
+    printf("Show the basis declaration of pointer\n");
+
+
+    printf("Here is m=%d, nand o are two integer variable and *z is  an integer\n",m);
+    printf("z stores the address of m=%d\n",&z);
+    printf("*z stores the value of m=%d\n",z);
+    printf("&m is the address of m=%d\n",&m);
+    printf("&n stores the address of n=%d\n",&n);
+    printf("&o stores the address of o=%d\n",&o);
+    printf("&z stores the address of z=%d\n",&z);
+
+
+
+
+
+
+    return 0;
+}
