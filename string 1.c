66b39d7b57da2338bf72770bc4465e36d630fd4b
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-12 04:19:54 -0700

    Add files via upload

---
+++ string 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{char a[50]=("Welcome,w3resource");
+    printf("Input the string:");
+    gets(a);
+    printf("The String you Entered is:%s",a);
+    return 0;
+}
