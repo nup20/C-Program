209e1da06729770eb862256b43ab1ef010037729
nup20 <130826435+nup20@users.noreply.github.com>
2023-06-12 04:20:17 -0700

    Add files via upload

---
+++ string 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{char a[50];
+int len;
+printf("Input the String=");
+    gets(a);
+len=strlen(a);
+printf("The Length of the Input string is=%d",len);
+
+
+    return 0;
+}
